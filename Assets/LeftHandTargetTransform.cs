4577c8b761d05ecd4b1fb0f120aeb54d016ddd34
nasty091 <6151071035@st.utc2.edu.vn>
2024-03-31 21:45:59 +0700

    Left hand IK - different weapon

---
+++ Assets/LeftHandTargetTransform.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftHandTargetTransform : MonoBehaviour
+{
+    // We need this script to detect game object
+    // We detecting left hand targets position and rotation
+}
--- Assets/WeaponVisualController.cs
+++ Assets/WeaponVisualController.cs
@@ -14,6 +14,16 @@ public class WeaponVisualController : MonoBehaviour
     [SerializeField] private Transform shotgun;
     [SerializeField] private Transform rifle;
 
+    private Transform currentGun;
+
+    [Header("Left hand IK")]
+    [SerializeField] private Transform leftHand;
+
+    private void Start()
+    {
+        SwitchOn(pistol);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -36,6 +46,9 @@ public class WeaponVisualController : MonoBehaviour
     {
         SwitchOffGuns();
         gunTransform.gameObject.SetActive(true);
+        currentGun = gunTransform;
+
+        AttachLeftHand();
     }
 
     private void SwitchOffGuns()
@@ -44,5 +57,12 @@ public class WeaponVisualController : MonoBehaviour
         {
             gunTransforms[i].gameObject.SetActive(false);
         }
+    }
+
+    private void AttachLeftHand()
+    {
+        Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTargetTransform>().transform;
+        leftHand.localPosition = targetTransform.localPosition;
+        leftHand.localRotation = targetTransform.localRotation;
     }
 }
