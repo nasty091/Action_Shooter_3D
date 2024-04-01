da7a067cc378dbbfac639eb12446b22ae383b503
nasty091 <6151071035@st.utc2.edu.vn>
2024-04-01 17:48:08 +0700

    Layer sync

--- Assets/WeaponVisualController.cs
+++ Assets/WeaponVisualController.cs
@@ -25,7 +25,7 @@ public class WeaponVisualController : MonoBehaviour
     {
         SwitchOn(pistol);
 
-        anim = GetComponentInParent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
