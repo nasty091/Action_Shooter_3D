0c782f874cf0ed25eb7cd288dc0d063dc1e0a482
nasty091 <6151071035@st.utc2.edu.vn>
2024-04-01 17:31:42 +0700

    Weapon animation layers

--- Assets/WeaponVisualController.cs
+++ Assets/WeaponVisualController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class WeaponVisualController : MonoBehaviour
 {
+    private Animator anim;
+
     [SerializeField] private Transform[] gunTransforms;
 
     [SerializeField] private Transform pistol;
@@ -22,24 +24,41 @@ public class WeaponVisualController : MonoBehaviour
     private void Start()
     {
         SwitchOn(pistol);
+
+        anim = GetComponentInParent<Animator>();
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
             SwitchOn(pistol);
-
+            SwitchAnimationLayer(1);
+        }    
+            
         if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
             SwitchOn(revolver);
+            SwitchAnimationLayer(1);
+        }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
+        {     
             SwitchOn(autoRifle);
+            SwitchAnimationLayer(1);
+        }
 
         if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
             SwitchOn(shotgun);
+            SwitchAnimationLayer(2);
+        }
 
-        if (Input.GetKeyDown(KeyCode.Alpha5))
+        if (Input.GetKeyDown(KeyCode.Alpha5)) 
+        { 
             SwitchOn(rifle);
+            SwitchAnimationLayer(3);
+        }
     }
 
     private void SwitchOn(Transform gunTransform)
@@ -53,7 +72,7 @@ public class WeaponVisualController : MonoBehaviour
 
     private void SwitchOffGuns()
     {
-        for(int i = 0; i < gunTransforms.Length; i++)
+        for(int i = 1; i < gunTransforms.Length; i++)
         {
             gunTransforms[i].gameObject.SetActive(false);
         }
@@ -64,5 +83,15 @@ public class WeaponVisualController : MonoBehaviour
         Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTargetTransform>().transform;
         leftHand.localPosition = targetTransform.localPosition;
         leftHand.localRotation = targetTransform.localRotation;
+    }
+
+    private void SwitchAnimationLayer(int layerIndex)
+    {
+        for(int i = 1; i < anim.layerCount; i++)
+        {
+            anim.SetLayerWeight(i, 0);
+        }
+
+        anim.SetLayerWeight(layerIndex, 1);
     }
 }
