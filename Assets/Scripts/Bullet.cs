5a269b6b8fc0119150e8436a3b6510e676e6a98b
nasty091 <6151071035@st.utc2.edu.vn>
2024-04-10 12:43:54 +0700

    Impact Visual Effect of a Bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactFX;
+
     private Rigidbody rb => GetComponent<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactFX(collision);
         Destroy(gameObject);
+    }
+
+    private void CreateImpactFX(Collision collision)
+    {
+        if (collision.contacts.Length > 0) // Find the first contact when bullet touch the other object
+        {
+            ContactPoint contact = collision.contacts[0];
+
+            GameObject newImpactFX = Instantiate(bulletImpactFX, contact.point, Quaternion.LookRotation(contact.normal));
+
+            Destroy(newImpactFX, 1f);
+        }
     }
 }
