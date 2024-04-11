2df5389b41e88acc8cc87867c2072d2c1947a51a
nasty091 <6151071035@st.utc2.edu.vn>
2024-04-11 21:14:28 +0700

    Weapon type

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,7 +1,18 @@
 
+public enum WeaponType
+{
+    Pistol,
+    Revolver,
+    AutoRifle,
+    Shotgun,
+    Rifle
+}
+
+
 [System.Serializable] // Makes class visible in the inspector
 public class Weapon 
 {
+    public WeaponType weaponType;
     public int ammo;
     public int maxAmmo;
 }
