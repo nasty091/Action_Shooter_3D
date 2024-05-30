c15e50ff007d29b0a418139e587cda5ab0ccd168
Pham An <phamcaodaian.9a1@gmail.com>
2024-05-30 15:43:01 +0700

    Balancing Range enemies

--- Assets/Scripts/Player/Player_Health.cs
+++ Assets/Scripts/Player/Player_Health.cs
@@ -25,6 +25,9 @@ public class Player_Health : HealthController
 
     private void Die()
     {
+        if (isDead)
+            return;
+
         isDead = true;
         player.anim.enabled = false;
         player.ragdoll.RagdollActive(true);
