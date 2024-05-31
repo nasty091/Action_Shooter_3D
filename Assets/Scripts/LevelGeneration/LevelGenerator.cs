dcc30d71886f002b7d6cbb8c5d0053efa15b41aa
Pham An <phamcaodaian.9a1@gmail.com>
2024-05-31 19:10:29 +0700

    Creation, position and rotation

---
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private List<Transform> levelParts;
+    [SerializeField] private SnapPoint nextSnapPoint;
+
+    private void Start()
+    {
+        GenerateNextLevelPart();
+    }
+
+    [ContextMenu("Create next level part")]
+    private void GenerateNextLevelPart()
+    {
+        Transform newPart = Instantiate(ChooseRandomPart());
+        LevelPart levelPartScript = newPart.GetComponent<LevelPart>();
+
+        levelPartScript.SnapAlignPartTo(nextSnapPoint);
+        nextSnapPoint = levelPartScript.GetExitPoint();
+    }
+
+    private Transform ChooseRandomPart()
+    {
+        int randomIndex = Random.Range(0, levelParts.Count);
+
+        Transform choosenPart = levelParts[randomIndex];
+
+        levelParts.RemoveAt(randomIndex);
+
+        return choosenPart;
+    }
+}
---
+++ Assets/Scripts/LevelGeneration/LevelPart.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPart : MonoBehaviour
+{
+    public void SnapAlignPartTo(SnapPoint targetSnapPoint)
+    {
+        SnapPoint entrancePoint = GetEntrancePoint();
+
+        AlignTo(entrancePoint, targetSnapPoint); // IMPORTANT: Alignment should be before position snapping
+        SnapTo(entrancePoint, targetSnapPoint);
+    }
+
+    private void AlignTo(SnapPoint ownSnapPoint, SnapPoint targetSnapPoint)
+    {
+        // Calculate the rotation offset between the level part's current rotation
+        // and it's own snap point's rotation. This help in fine-tuning the alignment later.
+        var rotationOffset = ownSnapPoint.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+        
+        // Set the level part's rotation to match the target snap point's rotation.
+        // This is the initial step to align the orientations of the two parts.
+        transform.rotation = targetSnapPoint.transform.rotation;
+
+        // Rotate the level part by 180 degrees around the Y-axis. This is necessary because the snap points
+        // are typically facing opposite directions, and this rotation aligns them to face each other correctly
+        transform.Rotate(0, 180, 0);
+
+        // Apply the previously calculated offset. This step fine-tunes the alignment by adjusting the
+        // level part's rotation to account for any initial difference in orientation between the level
+        // part's own snap point and the main body of the part.
+        transform.Rotate(0, -rotationOffset, 0);
+    }
+
+    private void SnapTo(SnapPoint ownSnapPoint, SnapPoint targetSnapPoint)
+    {
+        // Calculate the offset between the level part's current postion
+        // and it's own snap point's position. This offset represents the
+        // distance and direction from the level part's pivot to its snap point.
+        var offset = transform.position - ownSnapPoint.transform.position;
+
+        // Determine the new position for the level part. It's calculated by
+        // adding the previously computed offset to the target snap point's postion
+        // This effectively moves the level part so that its snap point aligns
+        // with the target snap point's position
+        var newPosition = targetSnapPoint.transform.position + offset;
+
+        // Update the level part's position to the newly calculated position be using snap points.
+        transform.position = newPosition;   
+    }
+
+    public SnapPoint GetEntrancePoint() => GetSnapPointOfType(SnapPointType.Enter);
+
+    public SnapPoint GetExitPoint() => GetSnapPointOfType(SnapPointType.Exit);
+
+    private SnapPoint GetSnapPointOfType(SnapPointType pointType)
+    {
+        SnapPoint[] snapPoints = GetComponentsInChildren<SnapPoint>();
+        List<SnapPoint> filteredSnapPoints = new List<SnapPoint>();
+
+        // Collect all snap points of the specified type
+        foreach(SnapPoint snapPoint in snapPoints)
+        {
+            if(snapPoint.pointType == pointType) 
+                filteredSnapPoints.Add(snapPoint);
+        }
+
+        // If there are matching snap points, choose one at random
+        if(filteredSnapPoints.Count > 0)
+        {
+            int randomIndex = Random.Range(0, filteredSnapPoints.Count);
+            return filteredSnapPoints[randomIndex];
+        }
+
+        // Return null if no matching snap points are found
+        return null;
+    }
+}
